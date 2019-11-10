380edc40f61621ecdfcc0757fa20a34eab4cd9f6
David Blain <drblain22@gmail.com>
2019-11-09 21:56:22 -0500

    Update momir.py

--- momir.py
+++ momir.py
@@ -7,6 +7,12 @@ import os
 import time
 
 def update(timediff=1, forceupdate=False):
+    """
+    Updates the cmcs folder if the difference in time between the current time and the time stored in lastupdate.txt is greater than timediff months.
+    
+    If forceupdate is True, always updates.
+    Creates cmcs folder containing json data if it does not exist otherwise.
+    """
     cur_time = time.time()
     try:
         with open("lastupdate.txt", "r") as read_file:
@@ -16,7 +22,7 @@ def update(timediff=1, forceupdate=False):
         forceupdate = True
 
     if forceupdate:
-        print('Updating...')
+        print("Updating...")
         print("Retrieving JSON data...")
         page = requests.get("https://mtgjson.com/json/AllCards.json")
         page_json = json.loads(page.content)
@@ -24,14 +30,12 @@ def update(timediff=1, forceupdate=False):
         print("Removing illegal cards and noncreatures...")
         bad_lst = []
         for entry in page_json:
-            # last condition changed from page_json[entry]['names'][0] != entry to "names" in page_json[entry]
-            # removed try: except: pass from around the if statement
             if not page_json[entry]["legalities"] or "Creature" not in page_json[entry]["types"] or "names" in page_json[entry]:
                 bad_lst.append(entry)
         for badentry in bad_lst:
             page_json.pop(badentry)
 
-        print("Creating cmcs folder...", end=" ")
+        print("Creating cmcs folder...")
         try:
             os.mkdir("./cmcs/")
         except:
@@ -39,10 +43,7 @@ def update(timediff=1, forceupdate=False):
         else:
             print("Success.")
 
-        print("Populating cmcs folder...", end=" ")
-        #This 'range(17)' has to be updated if any creature is released that costs more than 16
-        # cmcs_lst changed to empty dictionary
-        #cmcs_lst = [{} for i in range(17)]
+        print("Populating cmcs folder...")
         cmcs_lst = {}
         for item in page_json:
             card_cmc = int(page_json[item]["convertedManaCost"])
@@ -50,58 +51,69 @@ def update(timediff=1, forceupdate=False):
                 cmcs_lst[card_cmc][item] = page_json[item]
             else:
                 cmcs_lst[card_cmc] = {item : page_json[item]}
-            #cmcs_lst[card_cmc][item] = page_json[item]
-        #for num in range(len(cmcs_lst)):
         for mana_cost in cmcs_lst:
-            #with open("./cmcs/"+str(num)+".json", "w") as write_file:
             with open("./cmcs/"+str(mana_cost)+".json", "w") as write_file:
                 json.dump(cmcs_lst[mana_cost], write_file, indent=4)
-                #json.dump(cmcs_lst[num], write_file, indent=4)
 
         print("Done.")
         with open("lastupdate.txt", "w") as write_file:
             write_file.write(str(int(time.time())))
     else:
-        print('No update needed.')
+        print("No update needed.")
 
 def getCost():
+    """
+    Retrieves a number from the user.
+
+    Returns whatever value the user inputs (even if it isn't a number).
+    """
+    # TODO : update this function to use getch
     num = int(input("Enter a converted mana cost: "))
-    # print("\n"*100)
     return num
 
 def getRandomCard(cost):
+    """
+    Uses the data in the cmcs folder to retrieve a random card of a certain converted mana cost.
+    """
     with open("./cmcs/"+str(cost)+".json", "r", encoding="utf-8") as read_file:
             data = json.load(read_file)
     return random.choice(list(data))
 
-def getImage(cardTitle, size='normal'):
+def getImage(cardTitle, size="normal"):
+    """
+    Gets a card image from the scryfall api and returns an image object that is dithered and resized to a receipt printer size.
+
+    Size can be set to either "small", "normal", or "large".
+    """
     page = requests.get("https://api.scryfall.com/cards/named?exact="+name)
     page_json = json.loads(page.content)
-    image_link = page_json['image_uris'][size]
-    #can change size to small, normal, or large
-    #GET IMAGE FROM IMAGE_LINK
+    image_link = page_json["image_uris"][size]
     image_response = requests.get(image_link)
     img = Image.open(BytesIO(image_response.content))
-    #resize to receipt width and convert to monochrome bitmap with dithering
-    return img.resize((384, 535)).convert('1')
+    return img.resize((384, 535)).convert("1")
 
 def printImage(imageObject):
-    #Print the image stored in imageObject using an attached USB receipt printer
+    """
+    Prints the image stored in imageObject using an attached USB receipt printer.
+
+    TODO
+    """
+    # TODO
     pass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     update()
 
     while True:
         try:
             cmc = getCost()
             name = getRandomCard(cmc)
+            # print(name) would not necessarily be present in the final product.
             print(name)
             img = getImage(name)
-            #Shows the image (This is where the receipt printer code would go)
-            #PRINT(img)
+            # Use of the receipt printer would replace im.show()
             img.show()
         except KeyboardInterrupt:
             break
         except:
-            print('Invalid Converted Mana Cost.')
+            print("Invalid Converted Mana Cost.")
